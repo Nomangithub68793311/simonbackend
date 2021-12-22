5ef3a489bd041074623f95d5a536129bbfbdbe29
unknown <rana ahamed>
2021-12-22 23:40:04 +0600

    aded

--- server.js
+++ server.js
@@ -1,26 +1,25 @@
-const express =require('express');
-const mongoose=require('mongoose')
+const express = require('express');
+const mongoose = require('mongoose')
 var cors = require('cors')
-const app=express();
-const router=require('./routes/authroute');
+const app = express();
+const router = require('./routes/authroute');
 app.use(cors())
 app.use(express.json());
 
-const mongouri='mongodb+srv://userSimon:SimonntHJ3322@cluster0.ckww6.mongodb.net/userData?retryWrites=true&w=majority';
+const mongouri = 'mongodb+srv://userSimon:SimonntHJ3322@cluster0.ckww6.mongodb.net/userData?retryWrites=true&w=majority';
 
 
-mongoose.connect(mongouri,{
-    useNewUrlParser:true,useUnifiedTopology:true
-}).then((result)=>{
-console.log('mongo connected');
+mongoose.connect(mongouri, {
+    useNewUrlParser: true, useUnifiedTopology: true
+}).then((result) => {
+    console.log('mongo connected');
 })
-.catch((err)=>{console.log(err)});
-app.get('/',(req,res)=>{
+    .catch((err) => { console.log(err) });
+app.get('/', (req, res) => {
     res.send('yes got it now');
-    // console.log('done')
 
 })
 
-const port=process.env.PORT || 5000;
-app.listen(port,()=>{console.log(`server run at ${port}`)})
+const port = process.env.PORT || 5000;
+app.listen(port, () => { console.log(`server run at ${port}`) })
 app.use(router)
