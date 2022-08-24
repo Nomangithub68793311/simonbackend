68763d249d636dfe5dfbccffdb6ac7145440a796
codecommit <rana.buddy@gmail.com>
2022-08-24 20:18:41 +0600

    added

--- server.js
+++ server.js
@@ -1,27 +1,14 @@
 const express = require('express');
-const http = require("http")
-const socketio = require('socket.io');
-const morgan = require('morgan');
-const { ExpressPeerServer } = require('peer');
+
+
 const mongoose = require('mongoose')
 var cors = require('cors')
 const app = express();
 const router = require('./routes/authroute');
 app.use(cors())
 app.use(express.json());
-const server = http.createServer(app)
-const io = socketio(server).sockets;
-const custogenFunc = () =>
-    (Math.random().toString(36) + "000000000000000000").substr(2, 16);
-const peerServer = ExpressPeerServer(server, {
-    debug: true,
-    path: "/",
-    generateClientId: custogenFunc
-})
-app.use("/mypeer", peerServer)
-io.on('connection', function (socket) {
-    console.log('socket connected');
-})
+
+
 // const mongouri='mongodb+srv://userSimon:SimonntHJ3322@cluster0.ckww6.mongodb.net/userData?retryWrites=true&w=majority';
 const mongouri = 'mongodb+srv://contact:My9J9xnpsYSRnH6@cluster0.ncmj4.mongodb.net/simonDatabase?retryWrites=true&w=majority'
 
